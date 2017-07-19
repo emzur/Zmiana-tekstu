418b03af1f1be2610913de7dbc99c67f86cb6f14
emzur <skrzynkafilipa@gmail.com>
2017-07-19 10:57:58 +0200

    zmiana tekstu

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+var animal = 'Zielone słonie';
+var animal = animal.toUpperCase();
+var text = text.replace('Papugi', animal);
+console.log(text.length/2);
+var text = text.substr(0, 80);
+console.log(text);
